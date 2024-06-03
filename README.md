0454d4a33f59d21103573d59f42ba6528a52235f
Ramunas Kazlauskas <akuyra1@gmail.com>
2024-06-03 23:56:54 +0100

    Finished assignment

--- README.md
+++ README.md
@@ -5,10 +5,27 @@ Image Gallery
 (Required)
 🎯 Please mention the requirements you met and which goals you achieved for this assignment.
 
+
+- One page, with a set of thumbnail images, and a larger image.
+
+- Styled appropriately with CSS, and make use of media queries.
+
+- Use client-side JavaScript to display the larger image when a thumbnail is selected.
+
+- Use client-side JavaScript to navigate between images using the keyboard (tab, enter).
+
+
 🎯 Were there any requirements or goals that you were not quite able to achieve?
+
+1) Couldn't make the images change when keyboard left/right arrows were pressed down. Still works when you use tab/enter.
+
+2) Did not attempt to create a voiceover / screen reader for image alt text.
 
 🎯 If so, could you please tell us what was it that you found difficult about these tasks?
 
+- 1) Couldn't figure out best way to make it work, tried using eventListeners but just couldn't make it work, I think i struggled with understanding where to declare what because of local and global scopes.
+
+- 2) Did not attempt because I ran out of time.
 
 
 
@@ -18,3 +35,13 @@ Image Gallery
 What went really well and what could have gone better?
 Detailing useful external sources that helped you complete the assignment (e.g Youtube tutorials).
 Describing errors or bugs you encountered while completing your assignment. 
+
+- Throughout the whole assignment i realised that I have basic knowledge of a lot of things but struggled to put everything together to make the logic of the project work how I need it to. The more code i added the more confused I got if something started not working.
+
+-I understood how essential planning stage is, the need to describe what needs to happen and only then start coding, I also did not use some of the things I should have like conditionals and loops.
+
+- Watched youtube channel "Traversy Media" which had a good Javascript crash course, helped with debugging my code.
+
+- Had a lot of errors around scope of variables and functions, felt like I needed a lot more practice with functions, scope and probably loops. 
+
+- Overall was a bit disappointed how well finished assignment looks and functions, but it highlighted where I need to improve. 
--- app.js
+++ app.js
@@ -1,83 +1,74 @@
 //create an array of objects (images and their attributes)
 let images = [
-    { src: "./Assets/art 1.jpg", alt: "What", id:1 },
-    { src: "./Assets/art 2.jpg", alt: "What", id:2 },
-    { src: "./Assets/art 3.jpg", alt: "What", id:3 },
-    { src: "./Assets/art 4.jpg", alt: "What", id:4 },
-    { src: "./Assets/Benten.jpg", alt: "What", id:5 },
-  ];
+  { src: "./Assets/art 1.jpg", alt: "Painting of a woman with blue hair and eyes, in a white dress with feathers and stars on it, purple background theme", id:1,},
+  { src: "./Assets/art 2.jpg", alt: "Painting of a woman with 2 white dowes, one on her hand, one on her shoulder", id:2 },
+  { src: "./Assets/art 3.jpg", alt: "Painting of a mystical woman with red hair and antlers coming out of her head, red long hair wrapped around the antlers.", id:3 },
+  { src: "./Assets/art 4.jpg", alt: "Painting of a women with white hair and red cheeks, love hearts painted all around the image, pink background theme", id:4 },
+  { src: "./Assets/Benten.jpg", alt: "Painting of an Asian woman playing a violin looking instrument, wearing a kimono", id:5 },
+];
+
 
 // # select the thubnail container to display the little images
 let thumbContainer = document.getElementById("thumbnail-container");
 console.log(thumbContainer)
 
-// # dom elements
+// target DOM element where big image will be displayed
 let largeImageContainer = document.getElementById("pic-container")
 console.log(largeImageContainer)
 
 
 //!stretch goal (completely optional): carousel or arrow keys
 //global variable to store the current img index for the big image
-//let currentIndex = 0;
+let currentIndex = 0;
+// console.log(currentIndex)
 
-//select the DOM elements that contain our images with a document method
-//select the thumbnail-container
-//select the large-image-container
+//create thumbnails and first large picture
+createThumbnails()
+createLargeImage(createThumbnails)
+
+//assign both next/previous image variables from the DOM tags
+const nextImage = document.getElementById("right-arrow");
+const prevImage = document.getElementById("left-arrow");
 
 //function to create my thumbnail images --> action
-function createThumbnails(thumbContainer) {
-    //a loop to create multiple images --> forEach /for loop
-    images.forEach((thumbnail, index) => {
-      //a document method that creates a DOM element (img)
-      let img = document.createElement("img");
-      console.log(img);
-      //assign a value to the img attributes (eg. img.src = "value")
-      img.src = images[index].src;
-      img.alt = images[index].alt;
-      img.width = images[index].width;
-      img.height = images[index].height;
-      //(optional) assign a class name to the img element (.className property)
-      img.classList.add("thumb-image");
-      //append the new elements to the DOM using a document method
-      thumbContainer.appendChild(img)
-
-      img.addEventListener("click", function() {
-        // largeImageContainer.createElement("img");
-        
-      })
-      
-      //thumbnail.addEventListener("click", function (){
-      //when the user clicks on the image, we are going to call the function that creates the large image
-      // })
-    });
-    // createLargeImage(thumbnail[index]);
+function createThumbnails() {
+  //a loop to create multiple images --> forEach /for loop
+  images.forEach((thumbnail, index) => {
+    //a document method that creates a DOM element (img)
+    var img = document.createElement("img");
+    console.log(img);
+    //assign a value to the img attributes (eg. img.src = "value")
+    img.src = images[index].src;
+    img.alt = images[index].alt;
+    //(optional) assign a class name to the img element (.className property)
+    img.classList.add("thumb-image");
+    //append the new elements to the DOM using a document method
+    thumbContainer.appendChild(img)
+  });
 }
-  createThumbnails(thumbContainer)
-  // createThumbnails(argument --> images)
-  
-  function createLargeImage(createThumbnails) {
-    //problem -->  the images appear one after the other (innerHTML = "")
-    //create an image element
-    let bigImg = document.createElement("img");
-    //(optional) add a classname
-    bigImg.classList.add("big-picture");
-    //set the src value
-    bigImg.src = images[0].src;
-    //set the alt value
-    bigImg.alt = images[0].alt;
-    //append the large image to the DOM
-    largeImageContainer.appendChild(bigImg);
-    console.log(bigImg)
-    //add an event listener for each image
 
-      };
+// Function to display the large image
+function createLargeImage() {
+  // Clear the previous image
+  largeImageContainer.innerHTML = "";
+  // Create and display the new large image
+  let bigImg = document.createElement("img");
+  bigImg.classList.add("big-picture");
+  bigImg.src = images[currentIndex].src;
+  bigImg.alt = images[currentIndex].alt;
+  largeImageContainer.appendChild(bigImg);
+};
 
+//create event listeners for both the next image and previous image buttons
+nextImage.addEventListener("click", function() {
+    currentIndex = (currentIndex + 1) % images.length;
+    createLargeImage();    
+});
 
-    createLargeImage()
-
-
-
-
+prevImage.addEventListener("click", function() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    createLargeImage();
+});
 
 
 
